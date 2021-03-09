cdfa42f08b936666a0011b56ae56e44eb12c7885
Jeferson <jroliveira.oliveira301@gmail.com>
2021-03-09 17:57:18 +0100

    msm8916-common: Don't build AICP LiveWall
    * There's no more power for this beauty, so remove to
    make our ancient gadget snapier
    
    Change-Id: Id16a53e949876cf651d9dcd0c063751bb5345822

--- msm8916.mk
+++ msm8916.mk
@@ -368,6 +368,9 @@ PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
 PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
 
+# Remove AICP LiveWallpaper
+EXCLUDE_LIVE_WALLPAPER := true
+
 $(call inherit-product-if-exists, vendor/motorola/msm8916-common/msm8916-common-vendor.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, vendor/aicp/config/common_full_phone.mk)
