df17a7d29093b25944aaa3339a1bb03302035a46
Amit Pundir <amit.pundir@linaro.org>
2021-04-10 12:48:22 +0530

    msm8916-common: Copy mediaswcodec.policy
    * W mediaswcodec: Could not read additional policy file '/vendor/etc/seccomp_policy/mediaswcodec.policy'
    
    Signed-off-by: Amit Pundir <amit.pundir@linaro.org>
    Change-Id: If49acc4c8a7cff40fa33ed7259ae63ea7fd9879b

--- msm8916.mk
+++ msm8916.mk
@@ -285,7 +285,8 @@ PRODUCT_PACKAGES += \
 # Seccomp
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:system/vendor/etc/seccomp_policy/mediacodec.policy \
-    $(LOCAL_PATH)/seccomp/mediaextractor-seccomp.policy:system/vendor/etc/seccomp_policy/mediaextractor.policy
+    $(LOCAL_PATH)/seccomp/mediaextractor-seccomp.policy:system/vendor/etc/seccomp_policy/mediaextractor.policy \
+    $(LOCAL_PATH)/seccomp/mediaswcodec-seccomp.policy:system/vendor/etc/seccomp_policy/mediaswcodec.policy
 
 # Sensors
 PRODUCT_PACKAGES += \
