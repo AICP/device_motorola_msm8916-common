aa7be6fb3f4bb1944a6d02b2e10b5a995d7ea040
Tony Jose <gmoto1179@gmail.com>
2021-06-20 22:28:38 +0530

    msm8916-common: overlay: minor cleanup
    Change-Id: I0f5421e4416f0217d3a25062b620ba9c567f7824

--- msm8916.mk
+++ msm8916.mk
@@ -21,8 +21,7 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay
 
 # These are the hardware-specific features
 PRODUCT_COPY_FILES += \
